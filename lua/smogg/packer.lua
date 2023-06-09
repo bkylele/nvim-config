3d4cd7f81f1c1d8ae4d83a5029c855af5ecbd9c2
Brian Le <lebk@uci.edu>
2023-06-09 12:44:32 -0700

    Added lsp-zero

--- lua/smogg/packer.lua
+++ lua/smogg/packer.lua
@@ -27,11 +27,40 @@ use {
 	  end,
   }
 
-
 use {
 	'preservim/nerdtree',
 	requires = { {'nvim-lua/plenary.nvim'} }
 }
+
+
+use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    requires = {
+        -- LSP Support
+        {'neovim/nvim-lspconfig'},             -- Required
+        {                                      -- Optional
+        'williamboman/mason.nvim',
+        run = function()
+            pcall(vim.cmd, 'MasonUpdate')
+        end,
+    },
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},     -- Required
+    {'hrsh7th/cmp-nvim-lsp'}, -- Required
+    {'L3MON4D3/LuaSnip'},     -- Required
+
+    {'hrsh7th/cmp-path'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/cmp-buffer'},
+    {'saadparwaiz1/cmp_luasnip'},
+    {'rafamadriz/friendly-snippets'},
+}
+}
+
+
 
 
 use('ThePrimeagen/harpoon')
--- lua/smogg/remap.lua
+++ lua/smogg/remap.lua
@@ -2,13 +2,13 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("n", "K", "gk")
+-- vim.keymap.set("n", "K", "gk")
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
-vim.keymap.set("n", "<C-J>", "<C-W><C-J>")
-vim.keymap.set("n", "<C-K>", "<C-W><C-K>")
-vim.keymap.set("n", "<C-L>", "<C-W><C-L>")
-vim.keymap.set("n", "<C-H>", "<C-W><C-H>")
+-- vim.keymap.set("n", "<C-J>", "<C-W><C-J>")
+-- vim.keymap.set("n", "<C-K>", "<C-W><C-K>")
+-- vim.keymap.set("n", "<C-L>", "<C-W><C-L>")
+-- vim.keymap.set("n", "<C-H>", "<C-W><C-H>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
